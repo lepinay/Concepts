d1f3e60146e34a2ae6dff7bc44eaef53034d98d5
lepinay <lepinay@gmail.com>
2013-04-19 00:49:34 +0200

    refacto

--- Vanilla/AngularJsClient/Controllers/Editor.js
+++ Vanilla/AngularJsClient/Controllers/Editor.js
@@ -1,9 +1,23 @@
+function FileSelected(bus, editor){
+    bus("FileSelected").subscribe(function (file) {
+        if (!editor.hasFileOpened(file)) {
+            var doc = fileOpener(file);
+            editor.addDocument(doc);
+            editor.selectDocument(file);
+            bus("DocumentLoaded").broadcast(doc);
+            bus("DocumentSelected").broadcast(doc);
+       }
+        self.selectEditor(file);
+    });
+}
 
 function editorController($scope) {
 	
 	var radio = require("radio");  
   
-    $scope.editor = new (require("Editor"))(fileOpener, radio, fileSaver);
+    $scope.editor = new (require("Editor"))(radio, fileSaver);
+    
+    FileSelected(radio, $scope.editor);
 
     $scope.onTabSelected = function( path ){
         $scope.editor.selectEditor(path);
